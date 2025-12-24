e751c6ae6a021a1d9aec276ea4561d115d0cccc1
karimalasan13-arch <karimalasan13@gmail.com>
2025-12-24 19:20:42 +0000

    Create user_session.py

---
+++ user_session.py
@@ -0,0 +1,7 @@
+import uuid
+import streamlit as st
+
+def get_user_id():
+    if "user_id" not in st.session_state:
+        st.session_state.user_id = str(uuid.uuid4())
+    return st.session_state.user_id
