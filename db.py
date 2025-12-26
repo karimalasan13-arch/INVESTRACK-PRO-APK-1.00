4083404543e692113150fa9a75ee3663a57cf81c
karimalasan13-arch <karimalasan13@gmail.com>
2025-12-26 23:56:04 +0000

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ import streamlit as st
 
 # Read secrets from Streamlit Cloud
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_ANON PUBLIC_KEY"]
+SUPABASE_KEY = st.secrets["SUPABASE_ANON_PUBLIC_KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
